7d426c19df35c658d36d699bf46d7bc320778075
ashimbhowmik <ashimbhowmik239@gmail.com>
2024-01-02 19:05:51 +0600

    [Dashboard new2]

--- src/components/Dashboard/GraphChart/GraphChart.js
+++ src/components/Dashboard/GraphChart/GraphChart.js
@@ -1,56 +1,57 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Chart from "react-apexcharts";
+// import Chart from "react-apexcharts";
 
 const GraphChart = ({ userData, orderData, appointmentData, productData }) => {
-  const [state, setState] = useState({
-    options: {
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: ["Total User", "Total Appointment", "Total Order"],
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [0, 0, 0],
-      },
-    ],
-  });
+  // const [state, setState] = useState({
+  //   options: {
+  //     chart: {
+  //       id: "basic-bar",
+  //     },
+  //     xaxis: {
+  //       categories: ["Total User", "Total Appointment", "Total Order"],
+  //     },
+  //   },
+  //   series: [
+  //     {
+  //       name: "series-1",
+  //       data: [0, 0, 0],
+  //     },
+  //   ],
+  // });
 
-  useEffect(() => {
-    const newData = [
-      userData?.length || 0, // Total number of users
-      appointmentData?.length || 0, // Total number of appointments
-      orderData?.length || 0, // Total number of orders
-    ];
+  // useEffect(() => {
+  //   const newData = [
+  //     userData?.length || 0, // Total number of users
+  //     appointmentData?.length || 0, // Total number of appointments
+  //     orderData?.length || 0, // Total number of orders
+  //   ];
 
-    setState((prev) => ({
-      ...prev,
-      series: [{ ...prev.series[0], data: newData }],
-    }));
-  }, [userData, orderData, appointmentData]);
-  const [dstate, setDstate] = useState({
-    options: {
-      series: [0, 0],
-      labels: ["Price Drop", "No Price Drop"],
-    },
-  });
+  //   setState((prev) => ({
+  //     ...prev,
+  //     series: [{ ...prev.series[0], data: newData }],
+  //   }));
+  // }, [userData, orderData, appointmentData]);
+  // const [dstate, setDstate] = useState({
+  //   options: {
+  //     series: [0, 0],
+  //     labels: ["Price Drop", "No Price Drop"],
+  //   },
+  // });
 
   return (
     <div>
       <section>
         <div>
-          {userData && orderData && appointmentData && (
+          {/* {userData && orderData && appointmentData && (
             <Chart
               options={state.options}
               series={state.series}
               type="bar"
               width="500"
             />
-          )}
+          )} */}
+          <h1>hi</h1>
         </div>
       </section>
     </div>
